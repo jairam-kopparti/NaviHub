0f9dbc9866935ec88bfcbeebed0158308772f9ab
jairam-kopparti <jairamkopparti@gmail.com>
2026-03-19 12:06:05 -0500

    Update page.tsx

--- navihub/src/app/page.tsx
+++ navihub/src/app/page.tsx
@@ -291,7 +291,7 @@ export default function Home() {
           marginBottom: "-100vh",
         }}
       >
-        {/* ================= SECTION 1 ================= */}
+        {/* ============= SECTION 1 =============== */}
         <section ref={section1Ref} className="min-h-screen bg-(--bg) flex items-center py-12 sm:py-24 relative z-20">
           <div className="max-w-6xl mx-auto px-4 sm:px-6 w-full">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16 items-center">
